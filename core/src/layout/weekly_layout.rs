66d5e6bf70df0239ab1e134a297fc5f6697198d7
minhduc <ducbv1407@gmail.com>
2026-04-19 22:01:55 +0200

    add: Define layout query/output contracts

--- core/src/layout/weekly_layout.rs
+++ core/src/layout/weekly_layout.rs
@@ -1,1 +1,7 @@
+use chrono::NaiveDate;
+use serde::{Deserialize, Serialize};
 
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct WeeklyLayoutQuery {
+    pub anchor_date: NaiveDate,
+}
